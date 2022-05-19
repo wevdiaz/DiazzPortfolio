fe47467a7bcab94cc27ac3185b448fae4e6cad62
Weverson Diazz <wevers.info@gmail.com>
2022-05-18 22:32:20 -0300

    aplicando funcionalidade para pegar o ano atual e marcar o link ativo na seção

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function getYearCurrent() {
+    const yearFooter = document.getElementById("yearCurrent");
+    const now = new Date();
+    const year = now.getFullYear();
+
+    yearFooter.textContent = year;
+}
+
+getYearCurrent();
+
+const navMenu = document.querySelectorAll(".menu li a");
+
+function checkPageCurrent(links) {
+    const urlPage = document.location.pathname.replace("/", "").split(".")[0]; 
+    
+    for (let link of links) {
+        
+        const nameLink = link.innerText.toLocaleLowerCase();
+
+            if (urlPage === nameLink) {
+                link.className = "active";
+            }
+            else {
+                link.className = ""
+            }        
+    }
+}
+
+checkPageCurrent(navMenu);
+
